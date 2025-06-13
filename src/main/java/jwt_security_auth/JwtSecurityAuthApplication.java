e5b19a7e68eaa307bceaad1fb18939b06b3d3ed5
sapopk <jooramos1@gmail.com>
2025-06-13 20:53:04 +0100

    Initial setup for JWT Auth Secutiry.

---
+++ src/main/java/jwt_security_auth/JwtSecurityAuthApplication.java
@@ -0,0 +1,11 @@
+package jwt_security_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSecurityAuthApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(JwtSecurityAuthApplication.class, args);
+	}
+}
---
+++ src/test/java/jwt_security_auth/JwtSecurityAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package jwt_security_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtSecurityAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
